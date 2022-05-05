6a1b1df106ed489dd90d65be0c2b87cdd744213a
mohdmeran <blackchoco3@gmail.com>
2022-05-05 22:34:50 +0800

    refactor path

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,8 +3,11 @@ const routes = [
     path: "/login/",
     component: () => import("layouts/LoginLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/LoginPage.vue") },
-      { path: "register", component: () => import("pages/RegisterPage.vue") },
+      { path: "", component: () => import("pages/Login/LoginPage.vue") },
+      {
+        path: "register",
+        component: () => import("pages/Login/RegisterPage.vue"),
+      },
       { path: "rolepage", component: () => import("pages/RolePage.vue") },
     ],
   },
